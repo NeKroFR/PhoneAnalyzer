28775754f4272e4d4336cbcab24aaba4a6d53e13
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-05-27 14:25:55 +0200

    Create PhoneAnalyzer.py

---
+++ PhoneAnalyzer.py
@@ -0,0 +1,65 @@
+import sys, re
+
+def gui():
+    banner = """\033[92m
+                                                                   
+ _____  _                   _____            _                     
+|  _  || |_  ___  ___  ___ |  _  | ___  ___ | | _ _  ___  ___  ___ 
+|   __||   || . ||   || -_||     ||   || .'|| || | ||- _|| -_||  _|
+|__|   |_|_||___||_|_||___||__|__||_|_||__,||_||_  ||___||___||_|  
+                                               |___|               
+"""
+    print(banner)
+    number = input("""\033[1;35mEnter the phone number to scan : \033[0m""")
+    return number
+
+def is_valid(phone_number):
+    """
+    verifie that the phone number is valid
+    """
+    cleaned_number = re.sub(r'\D', '', phone_number)
+    patterns = [
+        r'^\d{10}$',    # 10-digit national format
+        r'^\+\d{1,3}\d{4,14}$',     # International format with country code
+        r'^\(\d{1,3}\)\d{6,12}$',       # Format with regional access code
+        r'^\d{3}[-\s]\d{3}[-\s]\d{4}$'      # Format with spaces or dashes
+    ]
+
+    for pattern in patterns:
+        if re.match(pattern, cleaned_number):
+            return True
+    return False
+
+
+
+def format(number):
+    """
+    Number to a list of diferent formats of this number.
+    ---
+    EX:
+        IN: "06 18 34 22 14"
+        OUT: ["0618342214", "+33 6 18 34 22 14", "(06) 18 34 22 14", "06 18 34 22 14", "06-18-34-22-14"]
+    """
+    #todo
+    return [number]
+
+
+
+def analyse(number):
+    number = format(number)
+    print(number)
+
+if __name__ == "__main__":
+    a = len(sys.argv)
+    if a == 1:
+        number = gui()
+     
+    elif a == 2:
+        number = sys.argv[1]
+    else:
+        print("\033[0;31mError: too many arguments\033[0m")
+        exit()
+    if is_valid(number):
+        analyse(number)
+    else:
+        print("\033[0;31mError: invalid phone number\033[0m")
