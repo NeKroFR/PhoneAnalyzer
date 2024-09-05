76a6a8e6ea24ea511aea6f4b99e31afad1df2050
meow <alexandre.meunier221@gmail.com>
2024-09-05 19:00:11 +0200

    interface and search scripts modified

--- search.py
+++ search.py
@@ -25,7 +25,7 @@ class Search:
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/600.8.9 (KHTML, like Gecko) Version/8.0.8 Safari/600.8.9",
                 "Mozilla/5.0 (iPad; CPU OS 8_4_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12H321 Safari/600.1.4"
             ]
-
+            
     def generate_cookie(self):
         """Generate a base64 encoded cookie."""
         raw_cookie = f"\b\x01\x12\x1C\b\x01\x12\x12gws_{datetime.today().strftime('%Y%m%d')}-0_RC3\x1A\x02en \x01\x1A\x06\b\x80º¦±\x06"
@@ -39,20 +39,24 @@ class Search:
         headers = {
             "User-Agent": random.choice(self.user_agents),
             "Cookie": f"SOCS={self.generate_cookie()}",
-        }        
+        }      
         try:
             response = requests.get(url, headers=headers)
             response.raise_for_status()
         except requests.RequestException as e:
             print(f"Error fetching URL: {e}")
             return []
-        #print(f"Searching for {query}...")
-        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        print(f"\033[1;35mSearching for {query}:\033[0m")
+        
+        soup = BeautifulSoup(response.text, 'html.parser')  
         for link in soup.find_all("a"):
             href = link.get("href")
-            if href and href.startswith("/url?q="):
-                url = href.split("/url?q=")[1].split("&")[0]
+            identifier = "/url?esrc=s&q=&rct=j&sa=U&url="
+            if href and identifier in href:
+                url = href.split(identifier)[1]
                 if url.startswith("http") or url.startswith("https"):
-                    if not any(pattern in url for pattern in self.banned_patterns):
+                    if not any(pattern in url for pattern in self.banned_patterns) and not url in results:
                         results.append(url)
+
         return results
